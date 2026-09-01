e9e8655600778433d8a6002a4c6bc3509be13ace
Jimb Esser <wasteland@gmail.com>
2026-09-01 08:36:09 -0700

    add test for non-multiple-of-4 textures

--- test/index.js
+++ test/index.js
@@ -5,6 +5,22 @@ var toArrayBuffer = require('buffer-to-arraybuffer')
 var path = require('path')
 var fs = require('fs')
 
+function dxtSize(shape, bpp) {
+  var w = (shape[0] + 3) & ~3
+  var h = (shape[1] + 3) & ~3
+  return w * h * bpp
+}
+
+function checkLevel(t, format, image) {
+  t.deepEqual(image.length, dxtSize(image.shape, format === 'dxt1' ? 0.5 : 1), 'data size matches block size')
+}
+
+function checkLevels(t, result) {
+  for (var ii = 0; ii < result.images.length; ++ii) {
+    checkLevel(t, result.format, result.images[ii])
+  }
+}
+
 test('should parse DDS headers', function (t) {
   var data = fs.readFileSync(path.join(__dirname, 'fixtures', 'test-dxt1.dds'))
   var array = toArrayBuffer(data)
@@ -14,6 +30,7 @@ test('should parse DDS headers', function (t) {
   t.deepEqual(typeof result.flags, 'number', 'exports some flags')
   t.deepEqual(result.images[0], { length: 131072, offset: 128, shape: [ 512, 512 ] }, 'mipmap level 0')
   t.deepEqual(result.images[1], { length: 131072 / 4, offset: 128 + 131072, shape: [ 256, 256 ] }, 'mipmap level 1')
+  checkLevels(t, result);
   t.end()
 })
 
@@ -48,3 +65,15 @@ test('should parse DDS headers', function (t) {
   t.end()
 })
 
+test('should handle non-multiple-of-4', function (t) {
+  var data = fs.readFileSync(path.join(__dirname, 'fixtures', 'test-7x3-dxt1.dds'))
+  var array = toArrayBuffer(data)
+  var result = parse(array)
+  t.deepEqual(result.shape, [ 7, 3 ], 'size is correct')
+  t.deepEqual(result.format, 'dxt1', 'internal format')
+  t.deepEqual(typeof result.flags, 'number', 'exports some flags')
+  t.deepEqual(result.images.length, 1);
+  t.deepEqual(result.images[0].shape, [ 7, 3 ])
+  checkLevels(t, result)
+  t.end()
+})
